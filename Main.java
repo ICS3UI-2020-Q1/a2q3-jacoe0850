0f86f1ae9b16575f79520f2480353335158e1f09
jacoe0850 <jacoe0850@wrdsb.ca>
2020-09-15 12:50:24 +0000

    Finished ascending order

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Determines the ascending order of two numbers
+ * @jacoe0850 
  */
 public class Main {
 
@@ -9,7 +10,18 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+    // Asking for two integers
+    System.out.println("Please enter an integer");
+    int num = input.nextInt();
+    System.out.println("Please enter another integer");
+    int num2 = input.nextInt();
+    // Deciding and stating the ascending order of the two integers
+    if (num2 > num){
+      System.out.println("Your numbers in ascending order are, " + num + ", " + num2);
+    } else {
+      System.out.println("Your numbers in ascending order are, " + num2 + ", " + num);
+    }
     
   }
 }
